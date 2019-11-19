9258957eef9719b619c3fead199e4203278ca409
Rafael Marinho Cespedes <rafamola@gmail.com>
2019-11-19 18:02:36 -0300

    First commit

---
+++ src/test/java/com/invillia/banktestunitintegration/BankTestUnitIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invillia.banktestunitintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTestUnitIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
